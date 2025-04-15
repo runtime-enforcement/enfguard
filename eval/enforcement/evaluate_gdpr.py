3ec7152b151703c24779057587d43a0cf5daaacf
Leonardo Lima <leonardo.alfs@proton.me>
2025-04-15 09:06:08 +0200

    Tweaking evaluation

--- eval/enforcement/evaluate_gdpr.py
+++ eval/enforcement/evaluate_gdpr.py
@@ -4,38 +4,38 @@ run_experiments(
     option        = 'monpoly',
     benchmark     = 'gdpr',
     exe           = './monpoly.exe',
-    accelerations = [1e5, 2e5, 4e5, 8e5, 1.6e6, 3.2e6, 6.4e6, 1.28e7, 2.56e7, 5.12e7],
-    n             = 5,#10,
+    accelerations = [2.56e7, 5.12e7],
+    n             = 1,
     time_unit     = 24 * 3600,
-    only_graph    = True,
+    only_graph    = False,
 )
 
 run_experiments(
     option        = 'enfguard',
     benchmark     = 'gdpr',
     exe           = './enfguard.exe',
-    accelerations = [1e5, 2e5, 4e5, 8e5, 1.6e6, 3.2e6, 6.4e6, 1.28e7, 2.56e7, 5.12e7],
-    n             = 1,#10,
+    accelerations = [6.4e6, 1.28e7, 2.56e7],
+    n             = 1,
     time_unit     = 24 * 3600,
-    only_graph    = True,
+    only_graph    = False,
 )
 
 run_experiments(
     option        = 'whyenf',
     benchmark     = 'gdpr',
     exe           = './whyenf.exe',
-    accelerations = [1e5, 2e5, 4e5, 8e5, 1.6e6, 3.2e6, 6.4e6, 1.28e7, 2.56e7, 5.12e7],
-    n             = 1,#10,
+    accelerations = [2e5, 8e5, 3.2e6, 6.4e6, 2.56e7],
+    n             = 1,
     time_unit     = 24 * 3600,
-    only_graph    = True,
+    only_graph    = False,
 )
 
 run_experiments(
     option        = 'enfpoly',
     benchmark     = 'gdpr',
     exe           = './enfpoly.exe',
-    accelerations = [1e5, 2e5, 4e5, 8e5, 1.6e6, 3.2e6, 6.4e6, 1.28e7, 2.56e7, 5.12e7],
-    n             = 1,#10,
+    accelerations = [5.12e7],
+    n             = 1,
     time_unit     = 24 * 3600,
-    only_graph    = True,
+    only_graph    = False,
 )
